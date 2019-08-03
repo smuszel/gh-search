da9f8c162f8ab0d09c8ff77ba418184a452216bc
smuszel <stanislaw.muszel@gmail.com>
2019-08-03 13:49:26 +0200

    add e2e tests

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,10 +1,19 @@
-const selectors = {
-};
+// Trick to get the intellisense for cypress
+/** @type {import('../support/types'))} */
+const cyp = cy;
 
+// TODO consider running in prod env
 describe('Github search', () => {
 
     beforeEach(() => {
-        cy.visit('localhost:8080');
+        cyp.visit('localhost:8080');
     });
+
+    it('has a user search functionality', () => {
+        cyp.get('#search').type('sny');
+        cyp.get('.react-autosuggest__suggestions-container').should('contain.text', 'snyff');
+        cyp.get('.react-autosuggest__suggestions-container').click();
+        cyp.get('#user-details-login').should('contain.text', 'snyff');
+    })
 
 });
--- cypress/integration/visual.spec.js
+++ cypress/integration/visual.spec.js
@@ -1,3 +1,7 @@
+// We can use it to generate screenshots for various app state for different viewports
+// Later we can compare the diff between old and new
+// This way we avoid resizing manually to check the styles
+
 describe('Github search', () => {
     beforeEach(() => {
         cy.visit('localhost:8080');
@@ -6,4 +10,21 @@ describe('Github search', () => {
     it('has default view', () => {
         cy.matchImageSnapshot();
     });
+
+    it('has state for selection of users', () => {
+        cy.get('#search').type('sny');
+        cy.matchImageSnapshot();
+    })
+
+    it('has state for hovering over selection', () => {
+        cy.get('#search').type('sny');
+        cy.get('.react-autosuggest__suggestions-container').trigger('mouseover');
+        cy.matchImageSnapshot();
+    })
+
+    it('has state for selected user details', () => {
+        cy.get('#search').type('sny');
+        cy.get('.react-autosuggest__suggestions-container').click();
+        cy.matchImageSnapshot();
+    })
 });
