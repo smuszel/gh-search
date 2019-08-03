a951d26415d0ea712e8249841bccc40aaacc6bed
smuszel <stanislaw.muszel@gmail.com>
2019-08-03 13:48:27 +0200

    tweak eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const webpack = require('webpack');
 const HtmlPlugin = require('html-webpack-plugin');
 const path = require('path');
@@ -5,6 +6,13 @@ const path = require('path');
 const paths = {
     index: path.resolve(__dirname, 'src', 'index.tsx'),
     template: path.resolve(__dirname, 'src', 'index.html'),
+    usersFixture: path.resolve(__dirname, 'cypress', 'fixtures', 'users.json'),
+    userDetailsFixture: path.resolve(
+        __dirname,
+        'cypress',
+        'fixtures',
+        'userDetails.json',
+    ),
 };
 
 /** @type {webpack.Configuration} */
@@ -12,11 +20,21 @@ module.exports = {
     entry: {
         index: paths.index,
     },
+    devServer: {
+        before: app => {
+            app.get('/users/:name', (req, res) => {
+                return res.json(require(paths.userDetailsFixture));
+            });
+            app.get('/search/users', (req, res) => {
+                return res.json(require(paths.usersFixture));
+            });
+        },
+    },
     resolve: {
         extensions: ['.js', '.css', '.ts', '.tsx'],
     },
     module: {
-        rules: [{ test: /\.(js|ts|tsx)/, use: ['babel-loader'] }],
+        rules: [{ test: /\.(js|ts|tsx)/, use: 'babel-loader' }],
     },
     devtool: 'source-map',
     plugins: [
