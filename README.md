e75ef47be0d2f7b500e5746c91c3bda2e9817810
smuszel <stanislaw.muszel@gmail.com>
2019-08-03 14:11:19 +0200

    update visual testing

--- README.md
+++ README.md
@@ -19,7 +19,15 @@ Thanks to following features this can serve as a boilerplate for larger project
 
 ### How to run tests
 
+CLI:
 * `npm run start`
 * under separate window `npm run test`
+
+GUI: 
+* `npm run start`
+* under separate window `npm run cy`
+
+Be aware that https://github.com/cypress-io/cypress/issues/2102 is still opened
+so it is advised to runthe `visual.spec.js` either from gui or cli
 
 ### :point_right: [Demo](https://smuszel.github.io/gh-search/dist/)
--- cypress/integration/visual.spec.js
+++ cypress/integration/visual.spec.js
@@ -1,10 +1,13 @@
 // We can use it to generate screenshots for various app state for different viewports
 // Later we can compare the diff between old and new
 // This way we avoid resizing manually to check the styles
+// https://medium.com/norwich-node-user-group/visual-regression-testing-with-cypress-io-and-cypress-image-snapshot-99c520ccc595
 
 describe('Github search', () => {
     beforeEach(() => {
         cy.visit('localhost:8080');
+        // TODO loop through set of viewports 
+        cy.viewport(1280, 768)
     });
 
     it('has default view', () => {
